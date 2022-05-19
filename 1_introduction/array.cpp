6827d4db3119ac5d6a96a0aa9763436fe31a359a
Prabin Joshi <prabinjoshi68@gmail.com>
2022-05-19 13:21:18 +0545

    Use Of Array

---
+++ 1_introduction/array.cpp
@@ -0,0 +1,11 @@
+// A simple array to print names of three students
+
+#include <iostream>
+
+using namespace std;
+int main(){
+    char *students[3]={"Prabin","Siddhant","Shivam"};
+    cout<<"The element of index 0 is "<<students[0]<<endl;
+    cout<<"The element of index 1 is "<<students[1]<<endl;
+    cout<<"The element of index 2 is "<<students[2]<<endl;
+}
