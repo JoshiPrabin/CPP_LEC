56d58c2be63db7a2d457cc3b0a106abeb8fd80e8
Prabin Joshi <prabinjoshi68@gmail.com>
2022-06-08 13:27:31 +0545

    this pointer

--- lab_sheet_2/complex_numbers.cpp
+++ lab_sheet_2/complex_numbers.cpp
@@ -13,19 +13,29 @@ class complex{
             cout<<"\nEnter imaginary part: ";
             cin>>imaginary;
         }
-        double rsum(complex c1, complex c2){
-            double realSum = c1.real+c2.real;
-            return realSum;
+        complex sum(complex c1,complex c2){
+            complex result;
+            result.real = c1.real+c2.real;
+            result.imaginary = c1.imaginary+c2.imaginary;
+            return result;
         }
-        double isum(complex c1, complex c2){
-            double imagSum = c1.imaginary+c2.imaginary;
-            return imagSum;
+        void display(complex result){
+                cout<<"The sum is: "<<result.real<<"+i "<<result.imaginary<<endl;
         }
+        // double rsum(complex c1, complex c2){
+        //     double realSum = c1.real+c2.real;
+        //     return realSum;
+        // }
+        // double isum(complex c1, complex c2){
+        //     double imagSum = c1.imaginary+c2.imaginary;
+        //     return imagSum;
+        // }
 };
 
 int main(){
-    complex num1,num2,result;
+    complex num1,num2,result1;
     num1.input();
     num2.input();
-    cout<<"The sum is: "<<result.rsum(num1,num2)<<" + i"<<result.isum(num1,num2);
+    result1.display(result1.sum(num1,num2));
+    // cout<<"The sum is: "<<result.rsum(num1,num2)<<" + i"<<result.isum(num1,num2);
 }
