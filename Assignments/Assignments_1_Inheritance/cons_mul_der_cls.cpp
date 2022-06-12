5a28ff2da4ea48d77dc439e20f08247516ccb4a7
Prabin Joshi <prabinjoshi68@gmail.com>
2022-06-12 23:10:40 +0545

    Constructor/Destructor and extending operator

---
+++ Assignments/Assignments_1_Inheritance/cons_mul_der_cls.cpp
@@ -0,0 +1,28 @@
+//constructor in multiply derived class
+#include<iostream>
+using namespace std;
+
+class base_one{
+    public:
+        base_one(){
+            cout<<"base_one class constructor"<<endl;
+        }
+};
+
+class base_two{
+    public:
+        base_two(){
+            cout<<"base_two class constructor"<<endl;
+        }
+};
+
+class derived: public base_one,public base_two{
+    public:
+        derived(){
+            cout<<"derived class constructor";
+        }
+};
+
+int main(){
+    derived d;
+}
