7f04f538e5916eb64223540d8f7609fbeafc0d8c
Prabin Joshi <prabinjoshi68@gmail.com>
2022-07-11 06:15:00 +0545

    fibonacci by operator overloading(postfix)

---
+++ lab_sheet_5/fib_opr_ovr_pstfx.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class fibonacci{
+    private:
+        int num;
+    public:
+        fibonacci(int n){
+            num = n;
+        }
+        void operator ++(int){
+            num = num+1;
+        }
+};
+int main(){
+    int n;
+    cout<<"Enter the number of digits you want: ";
+    cin>>n;
+    fibonacci f(n);
+    int a=0,b=1,sum=0;
+    cout<<" "<<a<<" ";
+    cout<<" "<<b<<" ";
+    for(int i=2;i<n;i++){
+        sum=a+b;
+        cout<<" "<<sum<<" ";
+        a=b;
+        b=sum;
+}
+}
