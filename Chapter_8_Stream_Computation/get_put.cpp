39171c9b87b18f6be7ccaa373d0ff645717b06a8
Prabin Joshi <prabinjoshi68@gmail.com>
2022-07-20 11:13:36 +0545

    use of get and put

---
+++ Chapter_8_Stream_Computation/get_put.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char c;
+    cout<<"Enter a character: ";
+    cin.get(c);
+    cout<<"The entered character is: ";
+    cout.put(c);
+    return 0;
+}
