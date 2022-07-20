9285b913830bbb0630fb078aa688d88c463bd8ec
Prabin Joshi <prabinjoshi68@gmail.com>
2022-07-20 11:35:07 +0545

    use of get and put

--- Chapter_8_Stream_Computation/get_put.cpp
+++ Chapter_8_Stream_Computation/get_put.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
+#include<cstdio>
 using namespace std;
 int main(){
-    char c;
+    char c,str[25];
     cout<<"Enter a character: ";
     cin.get(c);
+    cout<<"Enter a string: ";
+    gets(str);
     cout<<"The entered character is: ";
     cout.put(c);
+    cout<<"The entered string is: ";
+    puts(str);
     return 0;
 }
---
+++ Chapter_8_Stream_Computation/use_of_get.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+const int MAX = 50;
+char str1[MAX];
+char str2[MAX];
+int main(){
+    cout<<"Enter a string terminated by newline: "<<endl;
+    cin.get(str1,MAX);
+    //read string until MAX character is reached or new line is reached.
+    cout<<"Enter multiline string terminated by" 
+}
