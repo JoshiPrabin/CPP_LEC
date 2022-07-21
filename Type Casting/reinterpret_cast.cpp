df156e2a79b443eed8bfc9ba4f55ea864d5d8319
Prabin Joshi <prabinjoshi68@gmail.com>
2022-07-21 13:39:26 +0545

    reinterpret casting

---
+++ Type Casting/reinterpret_cast.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 100;
+    cout<<"Value of address of i is: "<<&i<<endl;
+    int *p;
+
+    // p = reinterpret_cast<int *>(i);
+    // cout<<"Value of p is: "<<p<<endl;
+
+    int ivar;
+    double dvar;
+    int *pivar;
+    double *pdvar;
+    void *pvoid;
+
+    pivar = reinterpret_cast<int *>(&dvar);
+    pivar = reinterpret_cast<int *>(&ivar);
+
+    cout<<"pivar= "<<pivar<<endl;
+    cout<<"pivar= "<<&pivar<<endl;
+    cout<<"pivar= "<<pdvar<<endl;
+}
