ba5bc8d77bfa1d8b4cd0dc871cda419b19dd9568
Prabin Joshi <prabinjoshi68@gmail.com>
2022-07-21 13:18:30 +0545

    static type casting

--- Type Casting/static_cast.cpp
+++ Type Casting/static_cast.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 int main(){
-    int i;
+    int i,j;
     float f = 3.124f;
+    char c='a';
     i = f;
     cout<<"Value of float is: "<<f<<endl;
     cout<<"Value of converted integer is: "<<i<<endl;
 
     i = static_cast<int>(f);
+    j = static_cast<int>(c);
     cout<<"Value of integer is: "<<i<<endl;
+    cout<<"Value of integer is: "<<j<<endl;
     return 0;
 }
