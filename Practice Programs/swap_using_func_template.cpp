685424125193727e1b15374226f3080f6086ce52
Prabin Joshi <prabinjoshi68@gmail.com>
2022-07-27 22:21:35 +0545

    Swapping two values using function template

--- Practice Programs/swap_using_func_template.cpp
+++ Practice Programs/swap_using_func_template.cpp
@@ -1,22 +1,14 @@
 #include<iostream>
 using namespace std;
 template <class T>
-T swap_value(T& x, T& y)
-{
-    T t;
-    t = x;
-    x = y;
-    y = t;
-    return 0;
+void swap_value(T& a, T& b){
+    cout<<"Before swapping= "<<a<<" "<<b<<endl;
+    cout<<"After swapping= "<<b<<" "<<a<<endl;
 }
-  
-int main()
-{
-    int a = 10, b = 20;
-    swap_value(a, b);
-    cout << a << " " << b << endl;
-    char c1 = 'a', c2 = 'b';
-    swap_value(c1, c2);
-    cout << c1 << " " << c2 << endl;
-    return 0;
+
+int main(){
+    int a=2,b=3;
+    swap_value(a,b);
+    char c1='a',c2='b';
+    swap_value(c1,c2);
 }
