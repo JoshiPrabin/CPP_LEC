f32ae19b7da10ab05fcc9db23aa29f8ec8990c14
Prabin Joshi <prabinjoshi68@gmail.com>
2022-08-14 05:24:53 +0545

    stream operator overloading for complex numbers

---
+++ Practice Programs/stream_opov.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class complex{
+    int real,imag;
+
+    public:
+        complex(){
+            real = 1;
+            imag = 2;
+        }
+         friend istream& operator >>(istream&, complex obj);
+         friend ostream& operator <<(ostream&, complex obj);
+};
+istream& operator >>(istream& input, complex obj){
+    cout<<"\n enter real: ";
+    input>>obj.real;
+    cout<<"\n enter imag: ";
+    input>>obj.imag;
+}
+
+ostream& operator <<(ostream& output, complex obj){
+    cout<<"\n the complex number is: "<<obj.real<<"+ i"<<obj.imag<<endl;
+}
+
+int main(){
+    complex s;
+    cin>>s;
+    cout<<s; //prints the value specified in the constructor
+    return 0;
+}
