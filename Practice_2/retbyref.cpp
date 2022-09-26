dfe80b60078e39b9be283c5d3634494f79256063
Prabin Joshi <prabinjoshi68@gmail.com>
2022-09-26 20:52:37 +0545

    example of return by reference

---
+++ Practice_2/retbyref.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int &max(int &x, int &y){
+    if (x>y)
+    {
+        return x;
+    }
+    else{
+        return y;
+    }
+    
+}
+int main(){
+    int a=5,b=10;
+    cout<<"Before calling a = "<<a<<" b = "<<b<<endl;
+    max(a,b)=-5;
+    cout<<"After calling a = "<<a<<" b = "<<b<<endl;
+    return 0;
+}
